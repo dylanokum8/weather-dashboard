eb8a361800dd01d1396a2f3c399cea2ad64a36ea
Dylan Okum <dylanokum@icloud.com>
2022-05-15 16:49:01 -0400

    HTML structure completed

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+var cityEl = document.getElementById("search-input");
+var searchEl = document.getElementById("search-button");
+var nameEl = document.getElementById("city-name");
+var tempEl = document.getElementById("temperature");
+var windEl = document.getElementById("wind-speed");
+var humidityEl = document.getElementById("humidity");
+var uvEl = document.getElementById("uv-index");
+
+var getWeather = function(cityName) {
+
+}
