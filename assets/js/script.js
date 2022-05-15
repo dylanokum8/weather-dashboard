fcd8eb501ac57ba6ecb84e71ae22b36a6bd4437e
Dylan Okum <dylanokum@icloud.com>
2022-05-15 18:52:51 -0400

    added current date and time

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,7 +5,21 @@ var tempEl = document.getElementById("temperature");
 var windEl = document.getElementById("wind-speed");
 var humidityEl = document.getElementById("humidity");
 var uvEl = document.getElementById("uv-index");
+var searchedCities = [];
+
+
+var today = new Date();
+var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+var dateTime = date+' '+time;
+
+
+
 
 var getWeather = function(cityName) {
+const cityShown = document.getElementById("city-name")
 
+cityShown.textContent = city
+
+fetch
 }
